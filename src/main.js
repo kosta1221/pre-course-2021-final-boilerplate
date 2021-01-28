79332c1a704b6b223fda40fdeed8efddd6f33204
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-01-28 14:20:39 +0200

    Display todo's
    Add function for displaying todo's in html
    Add form id and created an event listener for form - using prevent default so that page doesn't refresh itself upon form submission

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 const controlSection = document.querySelector(".control-section");
 const viewSection = document.querySelector(".view-section");
-const addButton = document.querySelector("#addbutton");
+const addButton = document.querySelector("#add-button");
 const textInput = document.querySelector("#text-input");
+const todoForm = document.querySelector("#todo-form");
 const prioritySelector = document.querySelector("#priority-selector");
-const toDoList = [];
+const todoList = [];
 
 /* A function for padding numbers to 2 digits. This is necessary for Date.gethours(), Date.getMinutes, etc. */
 function twoDigits(number) {
@@ -32,16 +33,40 @@ function toMySqlFormat(date) {
 
 /* A function for creating todo tasks */
 function createTodo() {
-	let todoText = textInput.innerText;
+	let todoText = textInput.value;
 	let todoCreatedAt = toMySqlFormat(new Date());
 	let todoPriority = prioritySelector.value;
 
 	return { todoText, todoCreatedAt, todoPriority };
 }
 
-/* A function for adding todo's to todo list */
-function addTodo() {
-	toDoList.unshift(createTodo());
+/* A function for adding todo's to todo list and displaying them on the page */
+function addAndDisplayTodo() {
+	todoList.unshift(createTodo());
+	const todo = todoList[0];
+	console.log(todo);
+
+	const todoContainer = document.createElement("div");
+	todoContainer.classList.add("todo-container");
+	viewSection.appendChild(todoContainer);
+
+	const todoPriority = document.createElement("div");
+	todoPriority.classList.add("todo-priority");
+	todoContainer.appendChild(todoPriority);
+	todoPriority.innerText = todo.todoPriority;
+
+	const todoCreatedAt = document.createElement("div");
+	todoCreatedAt.classList.add("todo-created-at");
+	todoContainer.appendChild(todoCreatedAt);
+	todoCreatedAt.innerText = todo.todoCreatedAt;
+
+	const todoText = document.createElement("div");
+	todoText.classList.add("todo-text");
+	todoContainer.appendChild(todoText);
+	todoText.innerText = todo.todoText;
 }
 
-/* addButton.addEventListener("click", ); */
+todoForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	addAndDisplayTodo();
+});
