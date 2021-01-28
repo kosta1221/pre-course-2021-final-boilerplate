5eece41430cb16d1d3f10ca88228959f1f9f29f5
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-01-28 13:04:25 +0200

    Enabled converting fates to mySQL format
    Add 2 functions for converting dates to mySQL format.

--- src/main.js
+++ src/main.js
@@ -5,10 +5,35 @@ const textInput = document.querySelector("#text-input");
 const prioritySelector = document.querySelector("#priority-selector");
 const toDoList = [];
 
-/* A function for adding todo tasks */
+/* A function for padding numbers to 2 digits. This is necessary for Date.gethours(), Date.getMinutes, etc. */
+function twoDigits(number) {
+	if (0 <= number && number < 10) {
+		return "0" + number.toString();
+	}
+	return number.toString();
+}
+
+/* A function for converting date objects to my mySQL format strings */
+function toMySqlFormat(date) {
+	return (
+		date.getUTCFullYear() +
+		"-" +
+		twoDigits(1 + date.getUTCMonth()) +
+		"-" +
+		twoDigits(date.getUTCDate()) +
+		" " +
+		twoDigits(date.getUTCHours()) +
+		":" +
+		twoDigits(date.getUTCMinutes()) +
+		":" +
+		twoDigits(date.getUTCSeconds())
+	);
+}
+
+/* A function for creating todo tasks */
 function createTodo() {
 	let todoText = textInput.innerText;
-	let todoCreatedAt;
+	let todoCreatedAt = toMySqlFormat(new Date());
 	let todoPriority = prioritySelector.value;
 
 	return { todoText, todoCreatedAt, todoPriority };
