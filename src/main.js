4c0656e6ddf200ddac23c79659c39de4d1dfafa0
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-01-28 12:43:47 +0200

    add createTodo & addTodo functions
    still figuring out the template, this is not functional yet.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
 const controlSection = document.querySelector(".control-section");
 const viewSection = document.querySelector(".view-section");
+const addButton = document.querySelector("#addbutton");
+const textInput = document.querySelector("#text-input");
+const prioritySelector = document.querySelector("#priority-selector");
+const toDoList = [];
+
+/* A function for adding todo tasks */
+function createTodo() {
+	let todoText = textInput.innerText;
+	let todoCreatedAt;
+	let todoPriority = prioritySelector.value;
+
+	return { todoText, todoCreatedAt, todoPriority };
+}
+
+/* A function for adding todo's to todo list */
+function addTodo() {
+	toDoList.unshift(createTodo());
+}
+
+/* addButton.addEventListener("click", ); */
