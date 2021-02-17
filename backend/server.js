361816018ab1d3038f1a5085758367aaf5730e27
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-02-18 01:41:28 +0200

    add PUT request to /b/{id}
    PUT request to /b/{id} get in the body params updated object and return the updated object

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,15 @@ app.get("/b", (req, res) => {
 // Currently works on http://localhost:3000/b/todos
 app.get("/b/:id", (req, res) => {
 	const { id } = req.params;
-	fs.readFile(`./todos/${id}.json`, (err, data) => {
-		if (err) {
-			res.status(500).json({ message: "Internal server error", error: err });
-		} else res.send(data);
-	});
+	if (!fs.existsSync(`./todos/${id}.json`)) {
+		res.status(400).json({ message: "Invalid bin id" });
+	} else {
+		fs.readFile(`./todos/${id}.json`, (err, data) => {
+			if (err) {
+				res.status(500).json({ message: "Internal server error", error: err });
+			} else res.send(data);
+		});
+	}
 });
 
 // POST request to /b - create new object and return the new object
@@ -49,7 +53,28 @@ app.post("/b", (req, res) => {
 			if (err) {
 				res.status(500).json({ message: "Internal server error", error: err });
 			} else {
-				console.log(`New user id: ${id}`);
+				console.log(`New bin id: ${id}`);
+				res.status(201).json(body);
+			}
+		});
+	}
+});
+
+// PUT request to /b/{id} get in the body params updated object and return the updated object
+app.put("/b/:id", (req, res) => {
+	const { id } = req.params;
+	const { body } = req;
+	if (!fs.existsSync(`./todos/${id}.json`)) {
+		res.status(400).json({
+			message: "Bin id not found",
+		});
+	} else {
+		body.id = id;
+		fs.writeFile(`./todos/${id}.json`, JSON.stringify(body, null, 4), (err) => {
+			if (err) {
+				res.status(500).json({ message: "Internal server error", error: err });
+			} else {
+				console.log(`Updated bin id: ${id}`);
 				res.status(201).json(body);
 			}
 		});
