597e07ee60a6af6ec4957a78760566c6be20ed3b
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-02-23 00:31:36 +0200

    Add login page (non functional)

---
+++ src/login.js
@@ -0,0 +1,5 @@
+/* DOM Elements declaration */
+const loginButton = document.querySelector("#login-button");
+const signupButton = document.querySelector("#signup-button");
+const usernameInput = document.querySelector("#username-input");
+const passwordInput = document.querySelector("#password-input");
